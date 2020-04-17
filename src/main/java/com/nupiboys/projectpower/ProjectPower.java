6f1787311898072aef6c5247a07f3447baaaa18f
Nicholas Taylor <nicholas.taylor0000@gmail.com>
2020-04-16 21:41:00 -0400

    ProjectPower Fix
    Removed ExampleMod and changed ProejctPower.java class name for reference within mod.toml

--- src/main/java/com/nupiboys/projectpower/ProjectPower.java
+++ src/main/java/com/nupiboys/projectpower/ProjectPower.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod("ProjectPower")
+@Mod("projectpower")
 public class ProjectPower
 {
     
