904d2168632d3e885e23737e0f80dbe522cb3467
Noah Saylor <butterdragonfly.games@gmail.com>
2020-04-17 00:50:55 -0400

    Added Copper Ingot
    Added the Copper Ingot item, the json files don't seem to be working for some reason, the texture is in there but it doesn't seem like the mod can find it.

---
+++ src/main/java/com/nupiboys/projectpower/items/ItemBase.java
@@ -0,0 +1,12 @@
+package com.nupiboys.projectpower.items;
+
+import com.nupiboys.projectpower.ProjectPower;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+public class ItemBase extends Item {
+
+    public ItemBase() {
+        super(new Item.Properties().group(ItemGroup.MATERIALS));
+    }
+}
---
+++ src/main/java/com/nupiboys/projectpower/util/RegistryHandler.java
@@ -0,0 +1,22 @@
+package com.nupiboys.projectpower.util;
+
+import com.nupiboys.projectpower.ProjectPower;
+import com.nupiboys.projectpower.items.ItemBase;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, ProjectPower.MOD_ID);
+
+    public static void init() {
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    // Items
+    public static final RegistryObject<Item> COPPER_INGOT = ITEMS.register("copper_ingot", ItemBase::new);
+
+}
