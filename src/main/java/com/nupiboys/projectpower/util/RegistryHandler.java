37350a55755d942e550fac38b02c9256c2793adb
Noah Saylor <butterdragonfly.games@gmail.com>
2020-04-18 00:55:48 -0400

    Added More Items
    Added the rest of the ingots along with all of their respective textures, which can be changed later. Also added every current metal's dust, although those items don't have textures yet.

--- src/main/java/com/nupiboys/projectpower/util/RegistryHandler.java
+++ src/main/java/com/nupiboys/projectpower/util/RegistryHandler.java
@@ -16,7 +16,22 @@ public class RegistryHandler {
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
-    // Items
+    // Items ONLY
+    //Ingots
     public static final RegistryObject<Item> COPPER_INGOT = ITEMS.register("copper_ingot", ItemBase::new);
+    public static final RegistryObject<Item> SILVER_INGOT = ITEMS.register("silver_ingot", ItemBase::new);
+    public static final RegistryObject<Item> ALUMINUM_INGOT = ITEMS.register("aluminum_ingot", ItemBase::new);
+    public static final RegistryObject<Item> SCARIUM_INGOT = ITEMS.register("scarium_ingot", ItemBase::new);
+    public static final RegistryObject<Item> BURNIUM_INGOT = ITEMS.register("burnium_ingot", ItemBase::new);
 
+    //Dusts
+    public static final RegistryObject<Item> IRON_DUST = ITEMS.register("iron_dust", ItemBase::new);
+    public static final RegistryObject<Item> GOLD_DUST = ITEMS.register("gold_dust", ItemBase::new);
+    public static final RegistryObject<Item> DIAMOND_DUST = ITEMS.register("diamond_dust", ItemBase::new);
+    public static final RegistryObject<Item> SILVER_DUST = ITEMS.register("silver_dust", ItemBase::new);
+    public static final RegistryObject<Item> COPPER_DUST = ITEMS.register("copper_dust", ItemBase::new);
+    public static final RegistryObject<Item> ALUMINUM_DUST = ITEMS.register("aluminum_dust", ItemBase::new);
+    public static final RegistryObject<Item> SCARIUM_DUST = ITEMS.register("scarium_dust", ItemBase::new);
+    public static final RegistryObject<Item> BURNIUM_DUST = ITEMS.register("burnium_dust", ItemBase::new);
+    
 }
