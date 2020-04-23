7093be04151a9d3a30f0f01c3826f2f69f58f90e
Noah Saylor <butterdragonfly.games@gmail.com>
2020-04-22 20:23:54 -0400

    Added Blocks and Fixed Textures

--- src/main/java/com/nupiboys/projectpower/util/RegistryHandlerBlocks.java
+++ src/main/java/com/nupiboys/projectpower/util/RegistryHandlerBlocks.java
@@ -13,5 +13,14 @@ public class RegistryHandlerBlocks {
     public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, ProjectPower.MOD_ID);
     
     public static final RegistryObject<Block> COPPER_BLOCK = BLOCKS.register("copper_block", () -> new Block(Block.Properties.create(Material.IRON).hardnessAndResistance(3.0F, 3.0F)));
+    public static final RegistryObject<Block> COPPER_ORE = BLOCKS.register("copper_ore", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0F, 3.0F)));
+    public static final RegistryObject<Block> ALUMINUM_BLOCK = BLOCKS.register("aluminum_block", () -> new Block(Block.Properties.create(Material.IRON).hardnessAndResistance(3.0F, 3.0F)));
+    public static final RegistryObject<Block> ALUMINUM_ORE = BLOCKS.register("aluminum_ore", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0F, 3.0F)));
+    public static final RegistryObject<Block> SILVER_BLOCK = BLOCKS.register("silver_block", () -> new Block(Block.Properties.create(Material.IRON).hardnessAndResistance(3.0F, 3.0F)));
+    public static final RegistryObject<Block> SILVER_ORE = BLOCKS.register("silver_ore", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0F, 3.0F)));
+    public static final RegistryObject<Block> BURNIUM_BLOCK = BLOCKS.register("burnium_block", () -> new Block(Block.Properties.create(Material.IRON).hardnessAndResistance(3.0F, 3.0F)));
+    public static final RegistryObject<Block> BURNIUM_ORE = BLOCKS.register("burnium_ore", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0F, 3.0F)));
+    public static final RegistryObject<Block> SCARIUM_BLOCK = BLOCKS.register("scarium_block", () -> new Block(Block.Properties.create(Material.IRON).hardnessAndResistance(3.0F, 3.0F)));
+    public static final RegistryObject<Block> SCARIUM_ORE = BLOCKS.register("scarium_ore", () -> new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(3.0F, 3.0F)));
 	
 }
