12c9fbef0759ab14ddc66a974de2484097a21814
Noah Saylor <butterdragonfly.games@gmail.com>
2020-04-23 23:50:26 -0400

    Added Copper Tools and Armor

--- src/main/java/com/nupiboys/projectpower/util/RegistryHandlerItems.java
+++ src/main/java/com/nupiboys/projectpower/util/RegistryHandlerItems.java
@@ -1,8 +1,25 @@
 package com.nupiboys.projectpower.util;
 
+import java.util.function.Supplier;
+
 import com.nupiboys.projectpower.ProjectPower;
 
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.HoeItem;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.IItemTier;
 import net.minecraft.item.Item;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ShovelItem;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -29,4 +46,135 @@ public class RegistryHandlerItems {
     public static final RegistryObject<Item> SCARIUM_DUST = ITEMS.register("scarium_dust", () -> new Item(new Item.Properties().group(ProjectPower.TAB)));
     public static final RegistryObject<Item> BURNIUM_DUST = ITEMS.register("burnium_dust", () -> new Item(new Item.Properties().group(ProjectPower.TAB)));
     
+    //Tools
+    public static final RegistryObject<Item> COPPER_PICKAXE = ITEMS.register("copper_pickaxe", () -> new PickaxeItem(ModItemTier.COPPER, 3, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> COPPER_AXE = ITEMS.register("copper_axe", () -> new AxeItem(ModItemTier.COPPER, 9, 1.3F, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> COPPER_SWORD = ITEMS.register("copper_sword", () -> new SwordItem(ModItemTier.COPPER, 6, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> COPPER_SHOVEL = ITEMS.register("copper_shovel", () -> new ShovelItem(ModItemTier.COPPER, 1, 2.0F, new Item.Properties().group(ProjectPower.TAB))); 
+    public static final RegistryObject<Item> COPPER_HOE = ITEMS.register("copper_hoe", () -> new HoeItem(ModItemTier.COPPER, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
+    
+    //Armor
+    public static final RegistryObject<Item> COPPER_HELMET = ITEMS.register("copper_helmet", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.HEAD, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> COPPER_CHESTPLATE = ITEMS.register("copper_chestplate", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.CHEST, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> COPPER_LEGGINGS = ITEMS.register("copper_leggings", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.LEGS, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> COPPER_BOOTS = ITEMS.register("copper_boots", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.FEET, new Item.Properties().group(ProjectPower.TAB)));
+    
+    //Custom Item Tier
+    public enum ModItemTier implements IItemTier {
+    	//Materials
+    	COPPER(2, 230, 5.0F, 1.7F, 13, () -> {
+    		return Ingredient.fromItems(RegistryHandlerItems.COPPER_INGOT.get());
+    	});
+    	
+    	private final int HARVEST_LEVEL;
+    	private final int MAX_USES;
+    	private final float EFFICIENCY;
+    	private final float ATTACK_DAMAGE;
+    	private final int ENCHANTABILITY;
+    	private final LazyValue<Ingredient> REPAIR_MATERIAL;
+    	
+    	ModItemTier(int harvestIn, int maxUsesIn, float efficiencyIn, float attackDamageIn, int enchantibilityIn, Supplier<Ingredient> repairMaterialIn) {
+    		this.HARVEST_LEVEL = harvestIn;
+    		this.MAX_USES = maxUsesIn;
+    		this.EFFICIENCY = efficiencyIn;
+    		this.ATTACK_DAMAGE = attackDamageIn;
+    		this.ENCHANTABILITY = enchantibilityIn;
+    		this.REPAIR_MATERIAL = new LazyValue<>(repairMaterialIn);
+    	}
+
+		@Override
+		public int getMaxUses() {	
+			return this.MAX_USES;
+		}
+
+		@Override
+		public float getEfficiency() {
+			return this.EFFICIENCY;
+		}
+
+		@Override
+		public float getAttackDamage() {
+			return this.ATTACK_DAMAGE;
+		}
+
+		@Override
+		public int getHarvestLevel() {
+			return this.HARVEST_LEVEL;
+		}
+
+		@Override
+		public int getEnchantability() {
+			return this.ENCHANTABILITY;
+		}
+
+		@Override
+		public Ingredient getRepairMaterial() {
+			return this.REPAIR_MATERIAL.getValue();
+		}
+    }
+    
+    //Custom Armor Material
+    public enum ModArmorMaterial implements IArmorMaterial {
+    	//Materials
+    	COPPER(ProjectPower.MOD_ID + ":copper_layer",  10, new int[] {2, 4, 3, 1}, 12, SoundEvents.field_226124_Y_, 0.0F, () -> {
+    		return Ingredient.fromItems(RegistryHandlerItems.COPPER_INGOT.get());
+    	});
+    	
+    	private static final int[] MAX_DAMAGE_ARRAY = new int [] { 8, 8, 8, 8 };
+    	private final String NAME;
+    	private final int MAX_DAMAGE_FACTOR;
+    	private final int[] DAMAGE_REDUCTION_AMOUNT_ARRAY;
+    	private final int ENCHANTABILITY;
+    	private final SoundEvent SOUND_EVENT;
+    	private final float TOUGHNESS;
+    	private final LazyValue<Ingredient> REPAIR_MATERIAL;
+    	
+    	ModArmorMaterial(String nameIn, int maxDamageFactorIn, int[] damageReductionAmountIn, int enchantabilityIn, SoundEvent soundEventIn, float toughnessIn, Supplier<Ingredient> repairMaterialIn) {
+    		this.NAME = nameIn;
+    		this.MAX_DAMAGE_FACTOR = maxDamageFactorIn;
+    		this.DAMAGE_REDUCTION_AMOUNT_ARRAY = damageReductionAmountIn;
+    		this.ENCHANTABILITY = enchantabilityIn;
+    		this.SOUND_EVENT = soundEventIn;
+    		this.TOUGHNESS = toughnessIn;
+    		this.REPAIR_MATERIAL = new LazyValue<>(repairMaterialIn);
+    	}
+
+		@Override
+		public int getDurability(EquipmentSlotType slotIn) {
+			return MAX_DAMAGE_ARRAY[slotIn.getIndex()] * this.MAX_DAMAGE_FACTOR;
+		}
+
+		@Override
+		public int getDamageReductionAmount(EquipmentSlotType slotIn) {
+			return this.DAMAGE_REDUCTION_AMOUNT_ARRAY[slotIn.getIndex()];
+		}
+
+		@Override
+		public int getEnchantability() {
+			return this.ENCHANTABILITY;
+		}
+
+		@Override
+		public SoundEvent getSoundEvent() {
+			return this.SOUND_EVENT;
+		}
+		
+		@OnlyIn(Dist.CLIENT)
+		@Override
+		public Ingredient getRepairMaterial() {
+			return this.REPAIR_MATERIAL.getValue();
+		}
+
+		@Override
+		public String getName() {
+			return this.NAME;
+		}
+
+		@Override
+		public float getToughness() {
+			return this.TOUGHNESS;
+		}
+    	
+    }
+
 }
