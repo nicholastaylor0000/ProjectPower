d6e94ceb08c3cca23af9f7e38f3978d93a1cf666
Noah Saylor <butterdragonfly.games@gmail.com>
2020-05-03 12:01:45 -0400

    Added Tools
    Added all the of the tools code and armor. Can't get the custom materials to work, code is there but I can't figure it out, so all tools and armor are very similar.

--- src/main/java/com/nupiboys/projectpower/util/RegistryHandlerItems.java
+++ src/main/java/com/nupiboys/projectpower/util/RegistryHandlerItems.java
@@ -25,10 +25,10 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class RegistryHandlerItems {
-    
+
     // Items ONLY
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, ProjectPower.MOD_ID);
-    	
+
     //Ingots
     public static final RegistryObject<Item> COPPER_INGOT = ITEMS.register("copper_ingot", () -> new Item(new Item.Properties().group(ProjectPower.TAB)));
     public static final RegistryObject<Item> SILVER_INGOT = ITEMS.register("silver_ingot", () -> new Item(new Item.Properties().group(ProjectPower.TAB)));
@@ -45,34 +45,92 @@ public class RegistryHandlerItems {
     public static final RegistryObject<Item> ALUMINUM_DUST = ITEMS.register("aluminum_dust", () -> new Item(new Item.Properties().group(ProjectPower.TAB)));
     public static final RegistryObject<Item> SCARIUM_DUST = ITEMS.register("scarium_dust", () -> new Item(new Item.Properties().group(ProjectPower.TAB)));
     public static final RegistryObject<Item> BURNIUM_DUST = ITEMS.register("burnium_dust", () -> new Item(new Item.Properties().group(ProjectPower.TAB)));
-    
+
     //Tools
     public static final RegistryObject<Item> COPPER_PICKAXE = ITEMS.register("copper_pickaxe", () -> new PickaxeItem(ModItemTier.COPPER, 3, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
     public static final RegistryObject<Item> COPPER_AXE = ITEMS.register("copper_axe", () -> new AxeItem(ModItemTier.COPPER, 9, 1.3F, new Item.Properties().group(ProjectPower.TAB)));
     public static final RegistryObject<Item> COPPER_SWORD = ITEMS.register("copper_sword", () -> new SwordItem(ModItemTier.COPPER, 6, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
-    public static final RegistryObject<Item> COPPER_SHOVEL = ITEMS.register("copper_shovel", () -> new ShovelItem(ModItemTier.COPPER, 1, 2.0F, new Item.Properties().group(ProjectPower.TAB))); 
+    public static final RegistryObject<Item> COPPER_SHOVEL = ITEMS.register("copper_shovel", () -> new ShovelItem(ModItemTier.COPPER, 1, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
     public static final RegistryObject<Item> COPPER_HOE = ITEMS.register("copper_hoe", () -> new HoeItem(ModItemTier.COPPER, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
-    
+
+    public static final RegistryObject<Item> SILVER_PICKAXE = ITEMS.register("silver_pickaxe", () -> new PickaxeItem(ModItemTier.COPPER, 3, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> SILVER_AXE = ITEMS.register("silver_axe", () -> new AxeItem(ModItemTier.COPPER, 9, 1.3F, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> SILVER_SWORD = ITEMS.register("silver_sword", () -> new SwordItem(ModItemTier.COPPER, 6, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> SILVER_SHOVEL = ITEMS.register("silver_shovel", () -> new ShovelItem(ModItemTier.COPPER, 1, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> SILVER_HOE = ITEMS.register("silver_hoe", () -> new HoeItem(ModItemTier.COPPER, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
+
+    public static final RegistryObject<Item> ALUMINUM_PICKAXE = ITEMS.register("aluminum_pickaxe", () -> new PickaxeItem(ModItemTier.COPPER, 3, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> ALUMINUM_AXE = ITEMS.register("aluminum_axe", () -> new AxeItem(ModItemTier.COPPER, 9, 1.3F, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> ALUMINUM_SWORD = ITEMS.register("aluminum_sword", () -> new SwordItem(ModItemTier.COPPER, 6, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> ALUMINUM_SHOVEL = ITEMS.register("aluminum_shovel", () -> new ShovelItem(ModItemTier.COPPER, 1, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> ALUMINUM_HOE = ITEMS.register("aluminum_hoe", () -> new HoeItem(ModItemTier.COPPER, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
+
+    public static final RegistryObject<Item> BURNIUM_PICKAXE = ITEMS.register("burnium_pickaxe", () -> new PickaxeItem(ModItemTier.COPPER, 3, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> BURNIUM_AXE = ITEMS.register("burnium_axe", () -> new AxeItem(ModItemTier.COPPER, 9, 1.3F, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> BURNIUM_SWORD = ITEMS.register("burnium_sword", () -> new SwordItem(ModItemTier.COPPER, 6, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> BURNIUM_SHOVEL = ITEMS.register("burnium_shovel", () -> new ShovelItem(ModItemTier.COPPER, 1, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> BURNIUM_HOE = ITEMS.register("burnium_hoe", () -> new HoeItem(ModItemTier.COPPER, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
+
+    public static final RegistryObject<Item> SCARIUM_PICKAXE = ITEMS.register("scarium_pickaxe", () -> new PickaxeItem(ModItemTier.COPPER, 3, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> SCARIUM_AXE = ITEMS.register("scarium_axe", () -> new AxeItem(ModItemTier.COPPER, 9, 1.3F, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> SCARIUM_SWORD = ITEMS.register("scarium_sword", () -> new SwordItem(ModItemTier.COPPER, 6, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> SCARIUM_SHOVEL = ITEMS.register("scarium_shovel", () -> new ShovelItem(ModItemTier.COPPER, 1, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> SCARIUM_HOE = ITEMS.register("scarium_hoe", () -> new HoeItem(ModItemTier.COPPER, 2.0F, new Item.Properties().group(ProjectPower.TAB)));
+
     //Armor
     public static final RegistryObject<Item> COPPER_HELMET = ITEMS.register("copper_helmet", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.HEAD, new Item.Properties().group(ProjectPower.TAB)));
     public static final RegistryObject<Item> COPPER_CHESTPLATE = ITEMS.register("copper_chestplate", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.CHEST, new Item.Properties().group(ProjectPower.TAB)));
     public static final RegistryObject<Item> COPPER_LEGGINGS = ITEMS.register("copper_leggings", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.LEGS, new Item.Properties().group(ProjectPower.TAB)));
     public static final RegistryObject<Item> COPPER_BOOTS = ITEMS.register("copper_boots", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.FEET, new Item.Properties().group(ProjectPower.TAB)));
-    
+
+    public static final RegistryObject<Item> SILVER_HELMET = ITEMS.register("silver_helmet", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.HEAD, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> SILVER_CHESTPLATE = ITEMS.register("silver_chestplate", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.CHEST, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> SILVER_LEGGINGS = ITEMS.register("silver_leggings", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.LEGS, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> SILVER_BOOTS = ITEMS.register("silver_boots", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.FEET, new Item.Properties().group(ProjectPower.TAB)));
+
+    public static final RegistryObject<Item> ALUMINUM_HELMET = ITEMS.register("aluminum_helmet", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.HEAD, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> ALUMINUM_CHESTPLATE = ITEMS.register("aluminum_chestplate", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.CHEST, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> ALUMINUM_LEGGINGS = ITEMS.register("aluminum_leggings", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.LEGS, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> ALUMINUM_BOOTS = ITEMS.register("aluminum_boots", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.FEET, new Item.Properties().group(ProjectPower.TAB)));
+
+    public static final RegistryObject<Item> BURNIUM_HELMET = ITEMS.register("burnium_helmet", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.HEAD, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> BURNIUM_CHESTPLATE = ITEMS.register("burnium_chestplate", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.CHEST, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> BURNIUM_LEGGINGS = ITEMS.register("burnium_leggings", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.LEGS, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> BURNIUM_BOOTS = ITEMS.register("burnium_boots", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.FEET, new Item.Properties().group(ProjectPower.TAB)));
+
+    public static final RegistryObject<Item> SCARIUM_HELMET = ITEMS.register("scarium_helmet", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.HEAD, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> SCARIUM_CHESTPLATE = ITEMS.register("scarium_chestplate", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.CHEST, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> SCARIUM_LEGGINGS = ITEMS.register("scarium_leggings", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.LEGS, new Item.Properties().group(ProjectPower.TAB)));
+    public static final RegistryObject<Item> SCARIUM_BOOTS = ITEMS.register("scarium_boots", () -> new ArmorItem(ModArmorMaterial.COPPER, EquipmentSlotType.FEET, new Item.Properties().group(ProjectPower.TAB)));
+
     //Custom Item Tier
     public enum ModItemTier implements IItemTier {
     	//Materials
     	COPPER(2, 230, 5.0F, 1.7F, 13, () -> {
-    		return Ingredient.fromItems(RegistryHandlerItems.COPPER_INGOT.get());
+    			return Ingredient.fromItems(RegistryHandlerItems.COPPER_INGOT.get());
+      	});
+    	/*
+    	SILVER(2, 230, 5.0F, 1.7F, 13, () -> {
+    		return Ingredient.fromItems(RegistryHandlerItems.SILVER_INGOT.get());
     	});
-    	
+    	ALUMINUM(2, 230, 5.0F, 1.7F, 13, () -> {
+    		return Ingredient.fromItems(RegistryHandlerItems.ALUMINUM_INGOT.get());
+    	});
+    	SCARIUM(2, 230, 5.0F, 1.7F, 13, () -> {
+    		return Ingredient.fromItems(RegistryHandlerItems.SCARIUM_INGOT.get());
+    	});
+    	BURNIUM(2, 230, 5.0F, 1.7F, 13, () -> {
+    		return Ingredient.fromItems(RegistryHandlerItems.BURNIUM_INGOT.get());
+    	});
+      */
+
     	private final int HARVEST_LEVEL;
     	private final int MAX_USES;
     	private final float EFFICIENCY;
     	private final float ATTACK_DAMAGE;
     	private final int ENCHANTABILITY;
     	private final LazyValue<Ingredient> REPAIR_MATERIAL;
-    	
+
     	ModItemTier(int harvestIn, int maxUsesIn, float efficiencyIn, float attackDamageIn, int enchantibilityIn, Supplier<Ingredient> repairMaterialIn) {
     		this.HARVEST_LEVEL = harvestIn;
     		this.MAX_USES = maxUsesIn;
@@ -83,7 +141,7 @@ public class RegistryHandlerItems {
     	}
 
 		@Override
-		public int getMaxUses() {	
+		public int getMaxUses() {
 			return this.MAX_USES;
 		}
 
@@ -112,14 +170,30 @@ public class RegistryHandlerItems {
 			return this.REPAIR_MATERIAL.getValue();
 		}
     }
-    
+
     //Custom Armor Material
     public enum ModArmorMaterial implements IArmorMaterial {
     	//Materials
     	COPPER(ProjectPower.MOD_ID + ":copper_layer",  10, new int[] {2, 4, 3, 1}, 12, SoundEvents.field_226124_Y_, 0.0F, () -> {
     		return Ingredient.fromItems(RegistryHandlerItems.COPPER_INGOT.get());
     	});
-    	
+      /*
+    	SILVER(ProjectPower.MOD_ID + ":silver_layer",  10, new int[] {2, 4, 3, 1}, 12, SoundEvents.field_226124_Y_, 0.0F, () -> {
+    		return Ingredient.fromItems(RegistryHandlerItems.SILVER_INGOT.get());
+    	});,
+
+    	ALUMINUM(ProjectPower.MOD_ID + ":aluminum_layer",  10, new int[] {2, 4, 3, 1}, 12, SoundEvents.field_226124_Y_, 0.0F, () -> {
+    		return Ingredient.fromItems(RegistryHandlerItems.ALUMINUM_INGOT.get());
+    	});,
+
+    	SCARIUM(ProjectPower.MOD_ID + ":scarium_layer",  10, new int[] {2, 4, 3, 1}, 12, SoundEvents.field_226124_Y_, 0.0F, () -> {
+    		return Ingredient.fromItems(RegistryHandlerItems.SCARIUM_INGOT.get());
+    	});,
+
+    	BURNIUM(ProjectPower.MOD_ID + ":burnium_layer",  10, new int[] {2, 4, 3, 1}, 12, SoundEvents.field_226124_Y_, 0.0F, () -> {
+    		return Ingredient.fromItems(RegistryHandlerItems.BURNIUM_INGOT.get());
+    	});
+      */
     	private static final int[] MAX_DAMAGE_ARRAY = new int [] { 8, 8, 8, 8 };
     	private final String NAME;
     	private final int MAX_DAMAGE_FACTOR;
@@ -128,7 +202,7 @@ public class RegistryHandlerItems {
     	private final SoundEvent SOUND_EVENT;
     	private final float TOUGHNESS;
     	private final LazyValue<Ingredient> REPAIR_MATERIAL;
-    	
+
     	ModArmorMaterial(String nameIn, int maxDamageFactorIn, int[] damageReductionAmountIn, int enchantabilityIn, SoundEvent soundEventIn, float toughnessIn, Supplier<Ingredient> repairMaterialIn) {
     		this.NAME = nameIn;
     		this.MAX_DAMAGE_FACTOR = maxDamageFactorIn;
@@ -158,7 +232,7 @@ public class RegistryHandlerItems {
 		public SoundEvent getSoundEvent() {
 			return this.SOUND_EVENT;
 		}
-		
+
 		@OnlyIn(Dist.CLIENT)
 		@Override
 		public Ingredient getRepairMaterial() {
@@ -174,7 +248,7 @@ public class RegistryHandlerItems {
 		public float getToughness() {
 			return this.TOUGHNESS;
 		}
-    	
+
     }
 
 }
